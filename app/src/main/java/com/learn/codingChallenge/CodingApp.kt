e8f934c6bce60f9eaa9b9e09c6df46996c7614ef
naga prakash <nagaprakash2020@gmail.com>
2021-03-13 11:02:25 -0500

    Adding DI using HILT and provided a Gson dependency into Application level (SingletonComponent)

---
+++ app/src/main/java/com/learn/codingChallenge/CodingApp.kt
@@ -0,0 +1,8 @@
+package com.learn.codingChallenge
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CodingApp: Application() {
+}
---
+++ app/src/main/java/com/learn/codingChallenge/dependency/module/NetworkModule.kt
@@ -0,0 +1,19 @@
+package com.learn.codingChallenge.dependency.module
+
+import com.google.gson.Gson
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun providesGsonBuilder(): Gson = Gson()
+
+
+}
